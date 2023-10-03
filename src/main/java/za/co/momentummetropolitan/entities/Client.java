01db3e561c241a2259d2ce479a6b3784f6cfe847
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 09:16:57 +0200

    * Entity for client domain added

---
+++ src/main/java/za/co/momentummetropolitan/entities/Client.java
@@ -0,0 +1,71 @@
+package za.co.momentummetropolitan.entities;
+
+import java.time.LocalDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+public class Client {
+    @Id
+    private Long id;
+    
+    private String address;
+    @Column("client_name")
+    private String clientName;
+    @Column("mobile_number")
+    private String mobileNumber;
+    private String email;
+
+    @Column("date_of_birth")
+    private LocalDate dateOfBirth;
+
+    public Client() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
