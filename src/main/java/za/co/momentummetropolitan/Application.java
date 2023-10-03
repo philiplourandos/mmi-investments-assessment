b3ba2ae3cca26b1245d94fd92a957a14de83bfa9
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 10:08:42 +0200

    * Added security configuration for web endpoint

--- src/main/java/za/co/momentummetropolitan/Application.java
+++ src/main/java/za/co/momentummetropolitan/Application.java
@@ -2,12 +2,25 @@ package za.co.momentummetropolitan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import za.co.momentummetropolitan.enums.AuthoritiesConst;
 
 @SpringBootApplication
 public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-    
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity security) throws Exception {
+        security.authorizeHttpRequests(authz -> {
+            authz.requestMatchers(HttpMethod.GET, "/client/*")
+                    .hasAnyAuthority(AuthoritiesConst.CLIENT, AuthoritiesConst.BROKER);
+        });
+
+        return security.build();
+    }
 }
