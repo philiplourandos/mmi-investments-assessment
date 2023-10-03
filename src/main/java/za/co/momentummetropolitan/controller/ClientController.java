36fcf1315c2ef579366a02d28ccfd9d167eac011
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 15:51:44 +0200

    * Added method `getClientInvestments` to controller

--- src/main/java/za/co/momentummetropolitan/controller/ClientController.java
+++ src/main/java/za/co/momentummetropolitan/controller/ClientController.java
@@ -1,10 +1,12 @@
 package za.co.momentummetropolitan.controller;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import za.co.momentummetropolitan.dto.ClientFinancialProducts;
 import za.co.momentummetropolitan.dto.ClientInfoResponse;
 import za.co.momentummetropolitan.service.ClientService;
 
@@ -21,5 +23,16 @@ public class ClientController {
     @GetMapping("/{id}")
     public ResponseEntity<ClientInfoResponse> getClientInfo(@PathVariable("id") final Long id) {
         return ResponseEntity.ok(clientService.retrieveClientInfo(id));
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity getClientInvestments(@PathVariable("id") final Long id) {
+        final List<ClientFinancialProducts> clientProducts = clientService.retrieveClientProducts(id);
+
+        if (clientProducts.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(clientProducts);
+        }
     }
 }
