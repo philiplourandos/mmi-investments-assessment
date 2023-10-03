f8027ddf5abb08b36ca7179b33e1df4759a78e77
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 10:57:29 +0200

    * Added test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- valid user data
+INSERT INTO CLIENTS(id, client_name, mobile_number, email, address, date_of_birth)
+    VALUES(2600, 'Philip Lourandos', '0835550001', 'philip.lourandos@gmail.com', '235 Beach Rd, Sea Point, Cape Town, 8005', '1978-06-25');
