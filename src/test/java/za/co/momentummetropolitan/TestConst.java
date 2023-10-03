fae0d88c7ba37536cb0c399fd72cce44e7d044d2
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 11:06:02 +0200

    * Switched from `email` to `id`

--- src/test/java/za/co/momentummetropolitan/TestConst.java
+++ src/test/java/za/co/momentummetropolitan/TestConst.java
@@ -4,4 +4,6 @@ import org.testcontainers.utility.DockerImageName;
 
 public final class TestConst {
     public static final DockerImageName POSTGRES_IMAGE = DockerImageName.parse("postgres:16.0-alpine");
+
+    public static final String VALID_CLIENT_ID = "2600";
 }
