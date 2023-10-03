cbcc12fa4b4aaaa2bd46ad68261401402e31ce82
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 15:46:16 +0200

    * Added method `retrieveClientProducts`

--- src/main/java/za/co/momentummetropolitan/service/ClientService.java
+++ src/main/java/za/co/momentummetropolitan/service/ClientService.java
@@ -1,17 +1,22 @@
 package za.co.momentummetropolitan.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
+import za.co.momentummetropolitan.dto.ClientFinancialProducts;
 import za.co.momentummetropolitan.dto.ClientInfoResponse;
 import za.co.momentummetropolitan.exceptions.ClientIdlNotFoundException;
+import za.co.momentummetropolitan.repository.ClientFinancialProductRepository;
 import za.co.momentummetropolitan.repository.ClientRepository;
 
 @Service
 public class ClientService {
 
     private final ClientRepository clientRepo;
+    private final ClientFinancialProductRepository clientProductsRepo;
 
-    public ClientService(final ClientRepository clientRepo) {
+    public ClientService(final ClientRepository clientRepo, final ClientFinancialProductRepository clientProductsRepo) {
         this.clientRepo = clientRepo;
+        this.clientProductsRepo = clientProductsRepo;
     }
 
     public ClientInfoResponse retrieveClientInfo(final Long id) {
@@ -19,5 +24,11 @@ public class ClientService {
                 .map(m -> new ClientInfoResponse(m.getClientName(), m.getAddress(), 
                         m.getEmail(), m.getMobileNumber(), m.getDateOfBirth()))
                 .orElseThrow(() -> new ClientIdlNotFoundException(id));
+    }
+
+    public List<ClientFinancialProducts> retrieveClientProducts(final Long id) {
+        clientRepo.findById(id).orElseThrow(() -> new ClientIdlNotFoundException(id));
+
+        return clientProductsRepo.getClientProducts(id);
     }
 }
