6ce4ba4101e1b8494ddb76e4eb76d783bc29e7e9
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-03 15:53:19 +0200

    * Added client with no linked products

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,6 +1,8 @@
 -- valid user data
 INSERT INTO CLIENTS(id, client_name, mobile_number, email, address, date_of_birth)
     VALUES(2600, 'Philip Lourandos', '0835550001', 'philip.lourandos@gmail.com', '235 Beach Rd, Sea Point, Cape Town, 8005', '1978-06-25');
+INSERT INTO CLIENTS(id, client_name, mobile_number, email, address, date_of_birth)
+    VALUES(6667, 'James Pereria', '0835550002', 'james@gmail.com', '16 Marmion Rd, Oranjezicht, Cape Town, 8005', '1958-05-30');
 
 -- product data
 INSERT INTO FINANCIAL_PRODUCTS(name, type) VALUES('Local Unit Trusts', 'INVESTMENTS');
