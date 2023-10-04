43efacb12b546c148150e3393be9289627946082
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-04 07:15:45 +0200

    * Renamed class

--- assessment-service/src/main/java/za/co/momentummetropolitan/dto/ClientFinancialProduct.java
+++ assessment-service/src/main/java/za/co/momentummetropolitan/dto/ClientFinancialProduct.java
@@ -3,7 +3,7 @@ package za.co.momentummetropolitan.dto;
 import java.math.BigDecimal;
 import za.co.momentummetropolitan.enums.FinancialProductsEnum;
 
-public record ClientFinancialProducts(Long id, String productName,
+public record ClientFinancialProduct(Long id, String productName,
         FinancialProductsEnum type, BigDecimal balance) {
 
 }
