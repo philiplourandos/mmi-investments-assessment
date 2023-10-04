20a8dc7685cab066879c00bfcf4da5e40199fb67
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-04 07:13:21 +0200

    * Renamed method `getClientProducts` to `findClientProductsByClientId` * Added additional constants for column names being retrieved * Added method `findById`

--- assessment-service/src/main/java/za/co/momentummetropolitan/repository/ClientFinancialProductRepository.java
+++ assessment-service/src/main/java/za/co/momentummetropolitan/repository/ClientFinancialProductRepository.java
@@ -1,9 +1,11 @@
 package za.co.momentummetropolitan.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import za.co.momentummetropolitan.dto.ClientFinancialProducts;
+import za.co.momentummetropolitan.dto.ClientFinancialProduct;
+import za.co.momentummetropolitan.entities.ClientProduct;
 import za.co.momentummetropolitan.enums.FinancialProductsEnum;
 
 @Repository
@@ -12,6 +14,8 @@ public class ClientFinancialProductRepository {
     private static final String CLIENT_PRODUCT_NAME = "name";
     private static final String CLIENT_PRODUCT_TYPE = "type";
     private static final String CLIENT_PRODUCT_BALANCE = "balance";
+    private static final String CLIENT_PRODUCT_FINANCIAL_PRODUCT_ID = "financial_product_id";
+    private static final String CLIENT_PRODUCT_CLIENT_ID = "client_id";
     
     private final JdbcTemplate jdbc;
 
@@ -19,32 +23,57 @@ public class ClientFinancialProductRepository {
         this.jdbc = jdbc;
     }
 
-    public List<ClientFinancialProducts> getClientProducts(final Long id) {
-        return jdbc.query(
-                """
+    public List<ClientFinancialProduct> findClientProductsByClientId(final Long clientId) {
+        return jdbc.query("""
                 SELECT
-                  cp.id AS id,
-                  fp.name AS name,
-                  fp.type AS type,
-                  cp.balance AS balance
+                    cp.id AS id,
+                    fp.name AS name,
+                    fp.type AS type,
+                    cp.balance AS balance
                 FROM
-                  FINANCIAL_PRODUCTS fp,
-                  CLIENT_PRODUCTS cp,
-                  CLIENTS c
+                    FINANCIAL_PRODUCTS fp,
+                    CLIENT_PRODUCTS cp,
+                    CLIENTS c
                 WHERE
-                  cp.client_id = c.id
-                  AND
-                  cp.financial_product_id = fp.id
-                  AND
-                  c.id = ?
+                    cp.client_id = c.id
+                    AND
+                    cp.financial_product_id = fp.id
+                    AND
+                    c.id = ?
                 ORDER BY
-                  cp.id DESC
-                """, (pss) -> pss.setLong(1, id), (rs, i) -> {
-                    return new ClientFinancialProducts(
+                    cp.id DESC
+                """, (pss) -> pss.setLong(1, clientId), (rs, i) -> {
+                    return new ClientFinancialProduct(
                             rs.getLong(CLIENT_PRODUCT_ID),
                             rs.getString(CLIENT_PRODUCT_NAME), 
                             FinancialProductsEnum.valueOf(rs.getString(CLIENT_PRODUCT_TYPE)),
                             rs.getBigDecimal(CLIENT_PRODUCT_BALANCE));
                 });
+    }
+    
+    public Optional<ClientProduct> findById(final Long id) {
+        final List<ClientProduct> products = jdbc.query(
+                """
+                SELECT
+                    cp.id,
+                    cp.client_id,
+                    cp.financial_product_id,
+                    cp.balance
+                FROM
+                    CLIENT_PRODUCTS cp
+                WHERE
+                    cp.id = ?
+                """, (pss) -> pss.setLong(1, id), (rs, i) -> {
+                    return new ClientProduct(rs.getLong(CLIENT_PRODUCT_ID), 
+                            rs.getLong(CLIENT_PRODUCT_CLIENT_ID), 
+                            rs.getLong(CLIENT_PRODUCT_FINANCIAL_PRODUCT_ID), 
+                            rs.getBigDecimal(CLIENT_PRODUCT_BALANCE));
+                });
+        
+        if (products.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(products.get(0));
+        }
     }
 }
