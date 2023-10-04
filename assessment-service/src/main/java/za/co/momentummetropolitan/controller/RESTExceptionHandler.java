eaf307ec98e6117157e466fe0b458d586cc53bac
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-04 07:29:46 +0200

    * Added method `handleClientProductIdNotFound`

--- assessment-service/src/main/java/za/co/momentummetropolitan/controller/RESTExceptionHandler.java
+++ assessment-service/src/main/java/za/co/momentummetropolitan/controller/RESTExceptionHandler.java
@@ -1,18 +1,27 @@
 package za.co.momentummetropolitan.controller;
 
+import static java.lang.String.format;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import za.co.momentummetropolitan.exceptions.ClientIdlNotFoundException;
+import za.co.momentummetropolitan.exceptions.ClientProductIdNotFoundException;
 
 @ControllerAdvice
 public class RESTExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ClientIdlNotFoundException.class})
-    public ProblemDetail handleEmailNotFound(final ClientIdlNotFoundException exception) {
+    public ProblemDetail handleClientIdNotFound(final ClientIdlNotFoundException exception) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, 
-                String.format("Id: %d does not exsit", exception.getId()));
+                format("Id: [%d] does not exsit", exception.getId()));
+    }
+
+    @ExceptionHandler({ClientProductIdNotFoundException.class})
+    public ProblemDetail handleClientProductIdNotFound(final ClientProductIdNotFoundException exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                format("Client Product Id: [%d] does not exist", exception.getClientProductId()));
     }
 }
