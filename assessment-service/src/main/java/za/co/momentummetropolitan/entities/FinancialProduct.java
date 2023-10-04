412204e7361d5382254a51f788384257dbc90a45
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-04 09:32:53 +0200

    * Added entity for financial products

---
+++ assessment-service/src/main/java/za/co/momentummetropolitan/entities/FinancialProduct.java
@@ -0,0 +1,48 @@
+package za.co.momentummetropolitan.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import za.co.momentummetropolitan.enums.FinancialProductsEnum;
+
+@Table("financial_products")
+public class FinancialProduct {
+    @Id
+    private Long id;
+    private String  name;
+    @Column("product_type")
+    private FinancialProductsEnum productType;
+
+    public FinancialProduct() {
+    }
+
+    public FinancialProduct(Long id, String name, FinancialProductsEnum productType) {
+        this.id = id;
+        this.name = name;
+        this.productType = productType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FinancialProductsEnum getProductType() {
+        return productType;
+    }
+
+    public void setProductType(FinancialProductsEnum productType) {
+        this.productType = productType;
+    }
+}
