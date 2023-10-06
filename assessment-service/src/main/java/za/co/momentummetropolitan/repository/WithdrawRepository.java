9be21f76deb71c1dfd244eb8ec89e52f6f2f6313
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-06 15:07:40 +0200

    #3
    * Corrected type

--- assessment-service/src/main/java/za/co/momentummetropolitan/repository/WithdrawRepository.java
+++ assessment-service/src/main/java/za/co/momentummetropolitan/repository/WithdrawRepository.java
@@ -1,7 +1,8 @@
 package za.co.momentummetropolitan.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import za.co.momentummetropolitan.entities.Withdraw;
 
-public interface WithdrawRepository extends CrudRepository<WithdrawRepository, Long> {
+public interface WithdrawRepository extends CrudRepository<Withdraw, Long> {
     
 }
