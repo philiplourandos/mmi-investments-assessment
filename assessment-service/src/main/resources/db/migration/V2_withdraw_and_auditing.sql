8013a798108a9c47480e61a8ff4531c11b8e7855
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-06 08:34:55 +0200

    #2
    * Added `withdraw` and `withdraw_audit_tracking` tables

---
+++ assessment-service/src/main/resources/db/migration/V2_withdraw_and_auditing.sql
@@ -0,0 +1,14 @@
+CREATE TYPE withdraw_status AS ENUM('STARTED', 'EXECUTING', 'DONE');
+
+CREATE TABLE withdraw(
+    id BIGSERIAL PRIMARY KEY,
+    client_product_id BIGINT NOT NULL,
+    status withdraw_status NOT NULL DEFAULT 'STARTED',
+    amount DECIMAL(20,2) NOT NULL);
+    
+CREATE TABLE withdraw_audit_tracking(
+    id BIGSERIAL PRIMARY KEY,
+    withdraw_id BIGINT NOT NULL,
+    status withdraw_status NOT NULL,
+    event_created TIMESTAMP DEFAULT NOW(),
+    description VARCHAR(500));
