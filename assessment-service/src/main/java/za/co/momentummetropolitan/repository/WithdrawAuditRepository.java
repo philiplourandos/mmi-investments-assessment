cdf64d6e30bcb5d0ca9f8e6234328a6f78c6067e
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-06 15:49:38 +0200

    #3
    * Added retrieval methods for test case

--- assessment-service/src/main/java/za/co/momentummetropolitan/repository/WithdrawAuditRepository.java
+++ assessment-service/src/main/java/za/co/momentummetropolitan/repository/WithdrawAuditRepository.java
@@ -1,8 +1,9 @@
 package za.co.momentummetropolitan.repository;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import za.co.momentummetropolitan.entities.WithdrawAuditTracking;
 
 public interface WithdrawAuditRepository extends CrudRepository<WithdrawAuditTracking, Long> {
-    
+    List<WithdrawAuditTracking> findByWithdrawIdOrderByEventCreatedAsc(Long withdrawId);
 }
--- assessment-service/src/main/java/za/co/momentummetropolitan/repository/WithdrawRepository.java
+++ assessment-service/src/main/java/za/co/momentummetropolitan/repository/WithdrawRepository.java
@@ -1,8 +1,9 @@
 package za.co.momentummetropolitan.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import za.co.momentummetropolitan.entities.Withdraw;
 
 public interface WithdrawRepository extends CrudRepository<Withdraw, Long> {
-    
+    Optional<Withdraw> findByClientProductId(Long clientProductId);
 }
