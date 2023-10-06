ac932b5f2d9d3ac87c08dc73af2c3812318e6d67
Philip Lourandos <philip.lourandos@gmail.com>
2023-10-06 12:33:08 +0200

    #2
    * Changed table names to uppercase
    * Added missing foreign key constraints

--- assessment-service/src/main/resources/db/migration/V2_withdraw_and_auditing.sql
+++ assessment-service/src/main/resources/db/migration/V2_withdraw_and_auditing.sql
@@ -1,14 +1,17 @@
 CREATE TYPE withdraw_status AS ENUM('STARTED', 'EXECUTING', 'DONE');
 
-CREATE TABLE withdraw(
+CREATE TABLE WITHDRAW(
     id BIGSERIAL PRIMARY KEY,
     client_product_id BIGINT NOT NULL,
     status withdraw_status NOT NULL DEFAULT 'STARTED',
     amount DECIMAL(20,2) NOT NULL);
     
-CREATE TABLE withdraw_audit_tracking(
+CREATE TABLE WITHDRAW_AUDIT_TRACKING(
     id BIGSERIAL PRIMARY KEY,
     withdraw_id BIGINT NOT NULL,
     status withdraw_status NOT NULL,
     event_created TIMESTAMP DEFAULT NOW(),
     previous_balance DECIMAL(20,2) NOT NULL;
+
+ALTER TABLE WITHDRAW ADD CONSTRAINT fk_client_product_id FOREIGN KEY (client_product_id) REFERENCES CLIENT_PRODUCTS(id);
+ALTER TABLE WITHDRAW_AUDIT_TRACKING ADD CONSTRAINT fk_withdraw_id FOREIGN KEY (withdraw_id) REFERENCES WITHDRAW(id);
